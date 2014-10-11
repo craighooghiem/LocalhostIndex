4999271547dfebe04fa85534fffc50599e145adb
Craig Hooghiem <craig@vicimus.com>
2014-10-11 17:01:13 -0400

    Making it a bit more functional. Still ugly

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
+<html>
+<head>
+	<title>Localhost Index</title>
+</head>
+<body>
+
+	<h1>
+		Localhost
+	</h1>
 <?php
 
-$directories = glob($somePath . '/*' , GLOB_ONLYDIR);
-$files = scandir();
+$directories = glob('/Applications/MAMP/htdocs/*', GLOB_ONLYDIR);
 
-print_r($directories);
-print_r($files);
+// Loop through the directories
+foreach($directories as $dir)
+{
+	$title = explode('/', $dir); $title = $title[count($title) - 1];
+	$files = scandir($dir);
 ?>
+	<h3><a href="<?php echo $title;?>"><?php echo $title; ?></a></h3>
+<?php
+	foreach($files as $f)
+	{
+		if(is_dir($f))
+			echo $f;	
+	}
+}
+
+?>
+
+</body>
+</html>
